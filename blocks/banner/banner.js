bb570f201a536f77cb2b7b7f13255d32da5782ce
Chirag Agrawal <tac73352@gmail.com>
2025-12-23 18:38:32 +0530

    lint fixes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,4 +1,4 @@
-export default function decorate(block) {
+export default function decorate() {
   // Add any specific decoration if needed
   // For now, assuming the block structure is handled by CSS
 }
