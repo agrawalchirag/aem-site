92b4aee86588522ea71c0d34c8830659877e682b
Chirag Agrawal <tac73352@gmail.com>
2025-12-24 20:50:17 +0530

    performance improvement

--- blocks/content/content.js
+++ blocks/content/content.js
@@ -1,2 +1,8 @@
-export default function decorate() {
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  // Optimize images for responsive loading
+  block.querySelectorAll('picture > img').forEach((img) => {
+    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }, { width: '400' }, { width: '200' }]));
+  });
 }
