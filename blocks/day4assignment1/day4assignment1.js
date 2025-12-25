4543a8bd07f1a0c09850d3a3ddee93626b5801c5
Chirag Agrawal <tac73352@gmail.com>
2025-12-25 19:17:10 +0530

    fix lint issues

--- blocks/day4assignment1/day4assignment1.js
+++ blocks/day4assignment1/day4assignment1.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   // Add custom icon to specific buttons
-  block.querySelectorAll('a').forEach(link => {
+  block.querySelectorAll('a').forEach((link) => {
     const text = link.textContent.trim();
     if (text === 'Button Default' || text === 'Button Primary') {
       link.classList.add('custom-icon');
